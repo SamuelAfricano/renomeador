d68ec56d8f21abed3f1006f9ff37215726f06e32
Samuel K Africano <Kandumboafricano@gmail.com>
2022-10-01 23:19:52 +1200

    Adicionando os arquivos principais do app

---
+++ Renomeador/rprename/__main__.py
@@ -0,0 +1,11 @@
+import  sys
+
+from PyQt5.QtWidgets import QApplication
+
+from  .views import Window
+
+def main():
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
---
+++ Renomeador/rprename/views.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+from collections import deque
+from pathlib import Path
+
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QFileDialog, QWidget
+
+from .rename import Renamer
+from .ui.window import Ui_Window
+
+FILTERS = ";;".join(
+    (
+        "PNG  (*.png)",
+        "JPEG  (*.jpeg)",
+        "JPG  (*.jpg)",
+        "mp3 (*.mp3)",
+        "mp4 (*.mp4)",
+    )
+)
+
+
+class Window(QWidget, Ui_Window):
+    def __init__(self):
+        super().__init__()
+        self._files = deque()
+        self._filesCount = len(self._files)
+        self._setupUI()
+        self._connectSignalsSlots()
+
+    def _setupUI(self):
+        self.setupUi(self)
+
+    def _connectSignalsSlots(self):
+        self.loadFilesButton.clicked.connect(self.loadFiles)
+        self.renameFilesButton.clicked.connect(self.renameFiles)
+
+    def loadFiles(self):
+        self.dstFileList.clear()
+        if self.dirEdit.text():
+            initDir = self.dirEdit.text()
+        else:
+            initDir = str(Path.home())
+        files, filter = QFileDialog.getOpenFileNames(
+            self, "Escolhe os ficheiros", initDir, filter=FILTERS
+        )
+        if len(files) > 0:
+            fileExtension = filter[filter.index("*") : -1]
+            self.extensionLabel.setText(fileExtension)
+            srcDirName = str(Path(files[0]).parent)
+            self.dirEdit.setText(srcDirName)
+            for file in files:
+                self._files.append(Path(file))
+                self.srcFileList.addItem(file)
+            self._filesCount = len(self._files)
+
+    def renameFiles(self):
+        self._runRenamerThread()
+
+    def _runRenamerThread(self):
+        prefix = self.prefixEdit.text()
+        self._thread = QThread()
+        self._renamer = Renamer(
+            files=tuple(self._files),
+            prefix=prefix,
+        )
+        self._renamer.moveToThread(self._thread)
+        # Renomeia
+        self._thread.started.connect(self._renamer.renameFiles)
+        # Actualize os saidas
+        self._renamer.renamerFile.connect(self._updateStateWhenFileRenamed)
+        self._renamer.progressed.connect(self._updateProgressBar)
+        # Limpa as saidas
+        self._renamer.finished.connect(self._thread.quit)
+        self._renamer.finished.connect(self._renamer.deleteLater)
+        self._thread.finished.connect(self._thread.deleteLater)
+        # inicie a thred
+        self._thread.start()
+
+    def _updateStateWhenFileRenamed(self, newFile):
+        self._files.popleft()
+        self.srcFileList.takeItem(0)
+        self.dstFileList.addItem(str(newFile))
+
+    def _updateProgressBar(self, fileNumber):
+        progressPercent = int(fileNumber / self._filesCount * 100)
+        self.progressBar.setValue(progressPercent)
